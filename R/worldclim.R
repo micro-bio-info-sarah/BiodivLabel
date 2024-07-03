930202874ae095977962a007fdd5441d85c0ff87
micro-bio-info-sarah <38938582+micro-bio-info-sarah@users.noreply.github.com>
2024-07-03 18:38:10 +0200

    Update CH4_from_manure_management

--- R/worldclim.R
+++ R/worldclim.R
@@ -1,34 +1,38 @@
 ### worldclim
 ### Extraction de donnees climatiques mensuelles
 ### A partir https://www.worldclim.org
-### Mohamed Hilal, CESAER, INRAE/Institut Agro
-### Mars 2024
+### Sarah HUET & Mohamed Hilal, CESAER, INRAE/Institut Agro
+### Juillet 2024
 
-rm(list=ls())
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-clc <- function() cat(rep("\n", 50))
-clc()
-install.packages("easypackages")
+#install.packages("easypackages")
 library(easypackages)
 
-listpack <- c("raster","sf","exactextractr",
+tmp_listpack <- c("raster","sf","exactextractr",
               "geodata",
               "data.table","tidyverse")
 
-needpack <- setdiff(listpack, rownames(installed.packages()))
-if(length(needpack)) {
-  install.packages(needpack)
+tmp_needpack <- setdiff(tmp_listpack, rownames(installed.packages()))
+if(length(tmp_needpack)) {
+  install.packages(tmp_needpack)
 }
 
-libraries(listpack)
+libraries(tmp_listpack)
 
-clc()
+# extract climate data ----
 
-# Pull departement boundaries for FRANCE
-ddep <- st_as_sf(gadm(country='FRA', level=2, path=tempdir())) %>%
+tmp_NUTS_clim <- tibble()
+
+for (tmp_country in tmp_country_codes$country_code) {
+
+# Pull departement boundaries for country
+tmp_ddep <- gadm(country=tmp_country, level=2, path=tempdir())
+
+if (!is.null(tmp_ddep)) {
+
+tmp_ddep <- st_as_sf(tmp_ddep) %>%
   mutate(DEP = CC_2,
          NOM_DEP = NAME_2) %>%
-  dplyr::select(DEP) %>%
+  #dplyr::select(DEP) %>%
   arrange(DEP)
 
 #Variable	Description	Unit
@@ -39,22 +43,34 @@ ddep <- st_as_sf(gadm(country='FRA', level=2, path=tempdir())) %>%
 #prec	total precipitation	mm
 #srad	incident solar radiation	kJ m-2 day-1
 #wind	wind speed (2 m above the ground)	m s-1
-varclim<-c('elev','tmin','tmax','tavg','prec','srad','wind')
+
+
+tmp_rclim <- worldclim_country(country = tmp_country, var='tavg', res=0.5, path=tempdir())
 
 # Calculate data frame of min, max, mean, median, mode for all month
-statclim <- c('min','max','mean','median','mode','sum')
+tmp_dclim <- exact_extract(tmp_rclim, tmp_ddep, 'mean')
+tmp_colClean <- gsub(paste0(".",tmp_country,"_wc2.1_30s_"), "_", colnames(tmp_dclim))
+colnames(tmp_dclim) <- tmp_colClean
 
-dataclim <- ddep %>%
-  dplyr::select(DEP) %>%
-  sf::st_drop_geometry()
+tmp_dataclim <- tmp_ddep %>%
+  #dplyr::select(DEP,NOM_DEP) %>%
+  dplyr::mutate(COUNTRY = tmp_country) %>%
+  sf::st_drop_geometry() %>%
+  cbind(.,tmp_dclim) %>%
+  mutate()
 
-for (v in varclim) {
-  print(v)
-  rclim <- worldclim_country(country = 'FRA', var=v, res=0.5, path=tempdir())
-  dclim <- exact_extract(rclim, ddep, statclim)
-  colClean <- gsub(".FRA_wc2.1_30s_", "_", colnames(dclim))
-  colnames(dclim) <- colClean
-  dataclim <- cbind(dataclim,dclim)
+tmp_NUTS_clim <- tmp_NUTS_clim %>%
+  rbind(.,tmp_dataclim)
+
 }
+
+}
+
+tmp_NUTS_clim <- tmp_NUTS_clim %>%
+  rowwise() %>%
+  mutate(annual_mean = mean(c_across(starts_with("mean_tavg"))))
+
+
+
 
 write.csv(dataclim,"dataclim.csv")
