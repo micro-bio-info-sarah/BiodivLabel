501d763aad07b554a787734a9324672af0513593
micro-bio-info-sarah <38938582+micro-bio-info-sarah@users.noreply.github.com>
2025-02-03 17:44:57 +0100

    First submission

--- huet_estimating_2025/result_tables.R
+++ huet_estimating_2025/result_tables.R
@@ -1,17 +1,18 @@
 # Result tables
 
-
+library(dplyr)
+library(tidyr)
 # Data ----
 
 #{r import_data} ----
 
 library(readr)
 
-stat_desc = readr::read_csv("~/BiodivLabel/CASD_export/CASD_export_241017/stat_desc_BVIAS_SIQO2024-10-17.csv")
+stat_desc = readr::read_csv("~/BiodivLabel/CASD_export/CASD_export_250122/stat_desc_BVIAS_SIQO_2025-01-22.csv")
 
-stat_tukey_hsd = readr::read_csv("~/BiodivLabel/CASD_export/CASD_export_241017/tmp_TukeyHSD_2024-10-17.csv")
+stat_tukey_hsd = readr::read_csv("~/BiodivLabel/CASD_export/CASD_export_250122//tmp_TukeyHSD_2025-01-22.csv")
 
-stat_paired_t_test = readr::read_csv("~/BiodivLabel/CASD_export/CASD_export_241017/tmp_paired_ttest_2024-10-17.csv")
+stat_paired_t_test = readr::read_csv("~/BiodivLabel/CASD_export/CASD_export_250122//tmp_paired_ttest_2025-01-22.csv")
 
 #
 
@@ -23,6 +24,7 @@ tmp_product_names <- tibble(
   product_name = c(
     "Ble_tendre",
     "Mais_grain",
+    "Mais_fourrage",
     "Triticale",
     "Melange_cereales_d_ete",
     "Autres_cereales",
@@ -34,6 +36,7 @@ tmp_product_names <- tibble(
   labels = c(
     "Soft Wheat",
     "Grain Maize",
+    "Forage maize",
     "Triticale",
     "Summer Cereal Mix",
     "Other Cereals",
@@ -105,68 +108,76 @@ library(dplyr)
 tmp_practice_names <- tibble(
   practice = rev(c("A.2.1",
                    "A.2.2",
+                   "A.3.1",
                    "A.3.2",
                    "A.3.3",
-                   "A.3.1",
                    "A.4.3",
                    "A.4.5",
                    "A.5.1",
-                   "yield")),
-  labels = rev(c("Hedge density (linear meter / ha)",
+                   "yield",
+                   "crop_nb_LU")),
+  labels = rev(c("Hedge Density (linear m / ha)",
                  "Mean Field Size (ha)",
-                 "Soil cover (number of uncovered day)",
-                 "Cultural Diversity (Shannon Index)",
                  "Tillage (L diesel / ha)",
-                 "Share of mineral fertilizer (%)",
-                 "Fertilization (kg N / ha)",
+                 "Soil Cover (Number of uncovered day)",
+                 "Crop Diversity (Shannon Index)",
+                 "Share of mineral fertilization (%)",
+                 "Nitrogen fertilization (kg N / ha)",
                  "Pesticides (€~TFI~UDNu / ha)",
-                 "Yield (kg / ha)")))
+                 "Yield (kg / ha)",
+                 "Number of crops")))
 tmp_practice_names$practice <- factor(tmp_practice_names$practice)
 names(tmp_practice_names$labels) <- tmp_practice_names$practice
 
 if (exists("practice_names")) {
   practice_names[["crops"]] <- tmp_practice_names
 } else {
-  practice_names = list()
+  practice_names <- list()
   practice_names[["crops"]] <- tmp_practice_names
 }
 
 # Herd ----
 
 tmp_practice_names <- tibble(
-  practice = c("yield_l_pha",
+  practice = c("yield_l_pha_ps", # pha_ps = per hectare of pseudofarm
+               "yield_l_pha",
                "yield_l_panim",
+               "nb_cow_pha_ps",
                "nb_cow_pha",
-               "cow_pMFA",
+               "nb_cow_pMFA",
+               "MFA_pcow",
+               "MFA_pha_ps",
+               "MFA_pha",
+               "ha_perm_grassland_pha_ps",
+               "ha_perm_grassland_pha",
+               "ha_temp_grassland_pha_ps",
+               "ha_temp_grassland_pha",
+               "protein_crop_ha_pha_ps",
+               "protein_crop_ha_pha",
                "feed_autonomy",
                "kg_DM_panim_maize_produced",
-               #"kg_DM_panim_soy_meal",
                "share_soybean",
-               #"share_concent_purchased",
-               "share_concent",
-               "protein_crop_ha_pha_pseudofarm",
-               "MFA_pcow",
-               "MFA_pha_pseudofarm",
-               "ha_temp_pasture_pha_pseudofarm",
-               "grassland_share_pseudofarm"#,
-               #"grassland_share_farm"
+               "share_concent"
   ),
-  labels = c("Yield (L milk / ha pseudofarm)",
-             "Yield (L milk / dairy cow)",
+  labels = c("Yield (L of milk / ha pseudofarm)",
+             "Yield (L of milk / ha farm)",
+             "Yield (L of milk / dairy cow)",
              "Livestock density (dairy cow / ha pseudofarm)",
+             "Livestock density (dairy cow / ha farm)",
              "Livestock density (dairy cow / ha MFA)",
-             "Feed autonomy (kg produced feed / kg feed)",
-             "Forage maize (kg produced forage maize / dairy cow)",
-             #"Soyben meal (kg purchased soybean meal / dairy cow)",
-             "Soybean meal (kg purchased soybean meal / kg feed)",
-             #"Part des concentrés dans les aliments achetés (kg de concentrés / kg d'aliments achetés)",
-             "Concentrate (kg concentrate / kg feed)",
-             "Legumes (ha legumes / ha pseudofarm",
              "Main Forage Area (MFA / dairy cow)",
-             "Main Forage Area (ha MFA / ha pseudofarm)",
-             "Temporary meadows (ha temp. meadow / ha pseudofarm)",
-             "Permanent grassland (ha perm. grassland / ha pseudofarm)"#,
-             #"Part des prairies permanentes (ha prairie / ha ferme)"
+             "Share of Main Forage Area (ha MFA / ha pseudofarm)",
+             "Share of Main Forage Area (ha MFA / ha farm)",
+             "Share of permanent grassland (ha permanent grassland / ha pseudofarm)",
+             "Share of permanent grassland (ha permanent grassland / ha farm)",
+             "Share of temporary grassland (ha temporary grassland / ha pseudofarm)",
+             "Share of temporary grassland (ha temporary grassland / ha farm)",
+             "Share of legumes (ha legumes / ha pseudofarm)",
+             "Share of legumes (ha legumes / ha farm)",
+             "Feed autonomy (kg produced feed / kg total feed)",
+             "Forage maize (kg of forage maize produced / dairy cow)",
+             "Share of purchased soybean meal (kg soybean meal / kg total feed)",
+             "Share of concentrate (kg concentrate / kg total feed)"
   ))
 
 tmp_practice_names$practice <- factor(tmp_practice_names$practice)
@@ -175,40 +186,51 @@ names(tmp_practice_names$labels) <- tmp_practice_names$practice
 if (exists("practice_names")) {
   practice_names[["herd"]] <- tmp_practice_names
 } else {
-  practice_names = list()
+  practice_names <- list()
   practice_names[["herd"]] <- tmp_practice_names
 }
 
 # Feed ----
+tmp_feed_origin <- tibble(
+  practice = c("feed_produced","feed_grassland","feed_purchased"),
+  labels = c(" among feed produced at farm"," among grasslands"," among purchased feed")
+)
 
-tmp_practice_names <- practice_names$crop %>%
-  mutate(practice = factor(paste0("feed_",practice)))
+tmp_practice_names <- tibble(
+  practice =
+    paste0(c("feed",tmp_feed_origin$practice),"_",rep(practice_names$crops$practice, each = length(c("feed",tmp_feed_origin$practice)))),
+  labels =
+    #rep(practice_names$crops$labels,times = length(c("feed",tmp_feed_origin)))
+    paste0(rep(practice_names$crops$labels,each = length(c(" among all feed",tmp_feed_origin$labels))),c(" among all feed",tmp_feed_origin$labels))
+) %>%
+  mutate(practice = factor(practice))
+
 names(tmp_practice_names$labels) <- tmp_practice_names$practice
+
 
 if (exists("practice_names")) {
   practice_names[["feed"]] <- tmp_practice_names
 } else {
-  practice_names = list()
+  practice_names <- list()
   practice_names[["feed"]] <- tmp_practice_names
 }
 
 # BVIAS ----
 
 tmp_practice_names <- tibble(
-  "practice" = c("BVIAS_ha","BVIAS_kg","BVIAS_t"),
+  "practice" = factor(x = c("BVI_ha","BVI_kg","BVI_t"),
+                      levels = c("BVI_ha","BVI_kg","BVI_t")),
   "labels" = c("BVIAS (ha)","BVIAS (kg)","BVIAS (t)"))
-tmp_practice_names$practice <- factor(tmp_practice_names$practice)
 names(tmp_practice_names$labels) <- tmp_practice_names$practice
 
 if (exists("practice_names")) {
   practice_names[["BVIAS"]] <- tmp_practice_names
 } else {
-  practice_names = list()
+  practice_names <- list()
   practice_names[["BVIAS"]] <- tmp_practice_names
 }
 
-# all ----
-# practice names
+#  all practice names ----
 tmp_practice_names <- Reduce(bind_rows,list(
   practice_names$BVIAS,
   practice_names$crops,
@@ -228,14 +250,28 @@ library(tidyr)
 tmp_table0 <- stat_desc %>%
   # add unmatched stat test
   left_join(.,stat_tukey_hsd %>%
+              # estimate % diff to conv
+              mutate(p100_diff_conv = -(conv_mean- trt_mean)/conv_mean*100) %>%
+              # select
               select(production_type, product_name, FQS, product_FQS, practice, practice_subset,
-                     stat_grp,stat_grp_conv,stat_grp_conv_diff,mean_conv_diff,pval_tukey) %>%
+                     p100_diff_conv,stat_grp,stat_grp_conv,stat_grp_conv_diff,pval_tukey) %>%
+
               mutate(match = "no_match"),
             by = join_by(production_type, product_name, FQS, product_FQS, practice, practice_subset,match)) %>%
   # add matching results
   left_join(.,stat_paired_t_test %>%
+              # estimate % diff to counterfactuals
+              left_join(
+                stat_paired_t_test %>%
+                  filter(FQS == "Conventionnel") %>%
+                  select(product_name, match, practice,mean) %>%
+                  rename(mean_counterfactual = mean),
+                by = join_by(product_name, match, practice)
+              ) %>%
+              mutate(p100_diff_counterfactuals = -(mean_counterfactual - mean)/mean_counterfactual*100) %>%
+              # select
               select(production_type, product_name, FQS, practice, match,
-                     mean_diff_match,pval_pttest,padjust_bonf),
+                     p100_diff_counterfactuals,pval_pttest,padjust_bonf),
             by = join_by(production_type, product_name, FQS, practice, match)) %>%
   # order factors
   mutate(
